ad9415e94cba9fdc027bae00b09071bcf4eede49
marvinjoel <marvinj.isd@gmail.com>
2021-08-17 00:27:44 -0500

    Juego consola: Trata de ganar piedra, papel o tijera

---
+++ piedra_papel_tijera.py
@@ -0,0 +1,49 @@
+import random
+
+
+
+def Regla_Juego():
+    print('============================')
+    print('  !Bienvenido al Juego!     ')
+    print('============================\n')
+    print("El juego consiste en lo siguiente:\nLa máquina tratarra de ganarte\nTienes que escoger la opción:\nPiedra, Papel, o Tijera.\nSuerte! :)\n\n")
+
+
+
+def Jugar():
+    Regla_Juego() #Llamamos a la funcion REGLA JUEGO
+
+    while True: 
+        user = input('Ingrese la opcion: ').lower()
+        lista = ['piedra','papel','tijera']
+        maquina = random.choice(lista)
+
+        #mesaje = "" if (user =='pi')else ""
+
+        if user in lista: #Se compara si el dato ingresado por el usuario está en la lista que asignamos
+            if user == maquina: #Comparacion si la opcion elegida por el usuario es igual a la opcion elegida por la máquina
+                return f'!Empate: sacaste: {user} y la saco {maquina}  :) '
+
+            if Juego_ganado(user, maquina): #Llamaremos a la funcion JUEGO_GANADO, que tiene 2 argumentos
+                return f'!Ganaste: sacaste, {user} y la saco {maquina}  :) '
+            else:
+                return f'Perdiste: sacaste: {user} y la saco {maquina}  :) '
+        else:
+            print('ERROR: Solo tienes que ingresar las opciones, piedra, papel, tijera.\n')
+
+
+def Juego_ganado(jugador, oponente):#Recibe 2 parametros
+    # Retornar true (verdadero) si gana el jugador.
+    # Piedra gana a tijera (pi > ti)
+    # tijera gana a papel (ti > pa)
+    # papel gana a piedra (pa > pi)
+    if ((jugador=='piedra' and oponente=='tijera') or (jugador=='tijera' and oponente=='papel') or (jugador=='papel' and oponente=='piedra')):
+        return True
+    else:
+        return False
+
+
+a = Jugar()
+print(a)
+
+    
